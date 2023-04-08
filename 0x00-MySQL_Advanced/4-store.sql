eb339de86876782f895fc498e264a7a026775f92
vivian347 <vkinyua001@gmail.com>
2023-04-08 23:52:57 +0300

    0-5

---
+++ 0x00-MySQL_Advanced/4-store.sql
@@ -0,0 +1,7 @@
+-- Write a SQL script that creates a trigger that decreases the quantity of an item after adding a new order.
+
+-- Quantity in the table items can be negative.
+
+DELIMITER $$
+
+CREATE TRIGGER quantity_decreased AFTER INSERT ON orders FOR EACH ROW BEGIN UPDATE items SET quantity = quantity - NEW.number WHERE name = NEW.item_name; END;
---
+++ 0x00-MySQL_Advanced/5-valid_email.sql
@@ -0,0 +1,12 @@
+DELIMITER $$
+
+CREATE TRIGGER valid_email_reset
+BEFORE UPDATE ON users
+FOR EACH ROW
+BEGIN
+    IF NEW.email != OLD.email THEN
+        SET NEW.valid_email = 0;
+    END IF;
+END$$
+
+DELIMITER ;
