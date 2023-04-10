6f488cc910ee4da53d1c946b82849cdc639571dc
vivian347 <vkinyua001@gmail.com>
2023-04-10 02:29:04 +0000

    procedures

--- 0x00-MySQL_Advanced/7-average_score.sql
+++ 0x00-MySQL_Advanced/7-average_score.sql
@@ -8,7 +8,7 @@ BEGIN
     SET average_score = (
         SELECT AVG(score)
         FROM corrections
-        WHERE user_id = user_id
+        WHERE corrections.user_id = user_id
     )
     WHERE id = user_id;
 
