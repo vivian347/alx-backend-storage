6ed02b806e18b9a6aebfb6102740615930f4da01
vivian347 <vkinyua001@gmail.com>
2023-04-14 13:17:43 +0000

    get redis

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -5,9 +5,35 @@
 import redis
 from typing import Union, Callable, Optional
 from uuid import uuid4
+from functools import wraps
 
 UnionList = Union[str, bytes, int, float]
 
+
+def count_calls(method: Callable) -> Callable:
+    """decorator counts times methods of cache class are called"""
+    key = method.__qualname__
+
+    @wraps(method)
+    def wrapper(self, *args, **kwds):
+        """ wrapper for method"""
+        self._redis.incr(key)
+        return method(self, *args, **kwds)
+    return wrapper
+
+def call_history(method: Callable) -> Callable:
+    """" store the history of inputs and outputs for a particular fu"""
+    input_list = method.__qualname__ + ":inputs"
+    output_list = method.__qualname__ + ":outputs"
+
+    @wraps(method)
+    def wrapper(self, *args):
+        """wrapper for method"""
+        self._redis.rpush(input_list, str(args))
+        output = method(self, *args)
+        self._redis.rpush(output_list, output)
+        return output
+    return wrapper
 
 class Cache:
     """class Cache"""
@@ -16,6 +42,8 @@ class Cache:
         self._redis = redis.Redis()
         self._redis.flushdb()
 
+    @count_calls
+    @call_history
     def store(self, data: UnionList) -> str:
         """generate a random key, sore input data and return key"""
         my_key = str(uuid4())
